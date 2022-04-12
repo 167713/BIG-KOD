c90023f7ab16eb98e19a99b532c918263fca7c59
167713 <100697872+167713@users.noreply.github.com>
2022-04-12 13:32:10 +0000

    Do zrobienia: przeszukiwanie

--- Main.java
+++ Main.java
@@ -1,12 +1,51 @@
 import java.io.IOException;
-
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
+    int opt;
+    Scanner s = new Scanner(System.in);
+    while(true){
+      System.out.println("1. Dodaj sudenta");
+      System.out.println("2. Wyswietl wszystkich studentow");
+      System.out.println("3. Wyszukaj studenta");
+      opt=s.nextInt();
+        switch(opt){
+        case 1:
+        dodaj(); break;
+        case 2:
+        wyswietl(); break;
+        case 3:
+        szukaj(); break;
+        default: System.out.println("Brak opcji"); break;
+    }
+  }
+}
+    public static void dodaj() {
+    try {
+      Scanner st = new Scanner(System.in);
+      String im="", naz="", dat="";
+      int wiek;
+      System.out.println("Podaj imie studenta: ");
+      im=st.nextLine();
+      System.out.println("Podaj nazwisko studenta: ");
+      naz=st.nextLine();
+      System.out.println("Podaj wiek studenta: ");
+      wiek=st.nextInt();
+      System.out.println("Podaj date urodzenia studenta: ");
+      st.nextLine();
+      dat=st.nextLine();
+      Service1 s = new Service1();
+      s.addStudent(new Student(im, naz, wiek, dat));
+      System.out.println("Dodano studenta.");
+      System.out.println("");
+    } catch (IOException e) {
+
+    }
+  }
+  
+  public static void wyswietl() {
     try {
       Service1 s = new Service1();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
-
       var students = s.getStudents();
       for(Student current : students) {
         System.out.println(current.ToString());
@@ -15,4 +54,34 @@ class Main {
 
     }
   }
+
+  public static void szukaj() {
+    try {
+      int d;
+      String dana;
+      Scanner d = new Scanner(System.in);
+      System.out.println("1. Wyszukaj po imieniu");
+      System.out.println("2. Wyszukaj po imieniu");
+      d=st.nextInt();
+      switch(d){
+      case 1:{
+        System.out.println("Podaj imie studenta: ");
+        dana=st.nextInt();
+        Service1 scn = new Service1();
+        var wysz = scn.findStudentByName();
+        break;}
+      case 2:{
+        System.out.println("Podaj nazwisko studenta: ");
+        dana=st.nextInt();
+        Service1 scs = new Service1();
+        var wysz = scs.findStudentBySname();
+        break;}
+      default: System.out.println("Brak opcji");
+      }
+    } catch (IOException e) {
+
+    }
+  }
+
+  
 }
--- Student.java
+++ Student.java
@@ -1,24 +1,30 @@
 public class Student {
 
   private String Name;
+  private String Sname;
   private int Age;
+  private String Dat;
 
-  public Student(String name, int age) {
+  public Student(String name, String sname, int age, String dat){
     Name = name;
+    Sname = sname;
     Age = age;
+    Dat = dat;
   }
 
   public String GetName() {return Name;}
+  public String GetSname() {return Sname;}
   public int GetAge() {return Age;}
+  public String GetDat() {return Dat;}
 
   public String ToString() {
-    return Name + " " + Integer.toString(Age);
+    return Name + " " + Sname + " " + Integer.toString(Age) + " " + Dat;
   }
 
   public static Student Parse(String str) {
     String[] data = str.split(" ");
-    if(data.length != 2) 
-      return new Student("Parse Error", -1);
-    return new Student(data[0], Integer.parseInt(data[1]));
+    if(data.length != 4) 
+      return new Student("Parse Error", "Parse Error", -1, "Parse Error");
+    return new Student(data[0], data[1], Integer.parseInt(data[2]), data[3]);
   }
 }
